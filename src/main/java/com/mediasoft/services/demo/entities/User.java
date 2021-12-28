43ea56a23a405f92d26edc9c3414b1d12a885779
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-12-27 19:46:59 -0500

    Creating User entity

--- src/main/java/com/mediasoft/services/demo/entities/User.java
+++ src/main/java/com/mediasoft/services/demo/entities/User.java
@@ -1,5 +1,46 @@
 package com.mediasoft.services.demo.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "tbl_users")
+@Data
+@AllArgsConstructor @NoArgsConstructor
+@Builder
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name="native", strategy = "native")
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+
+    @Transient
+    private String passwordConfirmation;
+
+    @Column(length = 512, nullable = true)
+    private String imgURL;
+    private Boolean status;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "tbl_user_roles",
+            joinColumns = @JoinColumn(name="user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+
 }

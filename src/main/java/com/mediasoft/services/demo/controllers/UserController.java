e093930630d5d93fdb084d8597fe06f43bcecb3f
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-12-29 09:45:42 -0500

    Configure UserController and refactoring views

--- src/main/java/com/mediasoft/services/demo/controllers/UserController.java
+++ src/main/java/com/mediasoft/services/demo/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.mediasoft.services.demo.controllers;
 
+import com.mediasoft.services.demo.entities.User;
+import com.mediasoft.services.demo.repositories.IRole;
 import com.mediasoft.services.demo.services.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +15,9 @@ public class UserController {
     @Autowired
     private IUserService service;
 
+    @Autowired
+    private IRole roleRepo;
+
     @GetMapping
     public String index() {
         return "index";
@@ -20,7 +25,10 @@ public class UserController {
 
     @GetMapping("user-form")
     public String user_form(Model model) {
+        model.addAttribute("userForm", new User());
         model.addAttribute("userList", service.getAllUsers());
+        model.addAttribute("roles", roleRepo.findAll());
+        model.addAttribute("listTab", "active");
         return "user-form/user-view";
     }
 }

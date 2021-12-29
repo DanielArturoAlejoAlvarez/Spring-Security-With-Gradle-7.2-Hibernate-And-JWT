3ccbd9af3169bc30f570b7ffcae2f68901bd4310
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-12-28 19:00:10 -0500

    Creating IUser repository

--- src/main/java/com/mediasoft/services/demo/repositories/IUser.java
+++ src/main/java/com/mediasoft/services/demo/repositories/IUser.java
@@ -1,8 +1,8 @@
 package com.mediasoft.services.demo.repositories;
 
-import com.mediasoft.services.demo.entities.Role;
+import com.mediasoft.services.demo.entities.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IRole extends CrudRepository<Role,Long> {}
+public interface IUser extends CrudRepository<User,Long> {}

37f5e964edc2150c7e5b2737aaee6acd39ed5f17
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-12-28 19:23:00 -0500

    Creating UserRoleImpl service to apply injection of dependency

---
+++ src/main/java/com/mediasoft/services/demo/services/RoleServiceImpl.java
@@ -0,0 +1,12 @@
+package com.mediasoft.services.demo.services;
+
+import com.mediasoft.services.demo.entities.Role;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleServiceImpl implements IRoleService{
+    @Override
+    public Iterable<Role> getAllRoles() {
+        return null;
+    }
+}

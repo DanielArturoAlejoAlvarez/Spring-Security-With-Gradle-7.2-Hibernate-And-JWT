35d96bd05502ea7db1409840179ec8f006f1ea8b
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-12-28 19:38:03 -0500

    Refactoring UserController using services and apply injection of dependency

--- src/main/java/com/mediasoft/services/demo/controllers/UserController.java
+++ src/main/java/com/mediasoft/services/demo/controllers/UserController.java
@@ -1,12 +1,17 @@
 package com.mediasoft.services.demo.controllers;
 
+import com.mediasoft.services.demo.services.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class UserController {
+    @Autowired
+    private IUserService service;
 
     @GetMapping
     public String index() {
@@ -14,7 +19,8 @@ public class UserController {
     }
 
     @GetMapping("user-form")
-    public String user_form() {
+    public String user_form(Model model) {
+        model.addAttribute("userList", service.getAllUsers());
         return "user-form/user-view";
     }
 }
--- src/main/java/com/mediasoft/services/demo/services/UserServiceImpl.java
+++ src/main/java/com/mediasoft/services/demo/services/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package com.mediasoft.services.demo.services;
 
 import com.mediasoft.services.demo.entities.User;
+import com.mediasoft.services.demo.repositories.IUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements IUserService{
+    @Autowired
+    private IUser repo;
+
     @Override
     public Iterable<User> getAllUsers() {
-        return null;
+        return repo.findAll();
     }
 }

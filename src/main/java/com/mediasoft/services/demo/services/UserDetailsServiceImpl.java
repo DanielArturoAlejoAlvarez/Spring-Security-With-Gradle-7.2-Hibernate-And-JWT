f5d72c5f24cf90813feb30e9d41a1846ec67554d
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-12-30 15:17:10 -0500

    Create UserDetailsServiceImpl service implements UserDetailsService to role user manager

---
+++ src/main/java/com/mediasoft/services/demo/services/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.mediasoft.services.demo.services;
+
+import com.mediasoft.services.demo.entities.Role;
+import com.mediasoft.services.demo.repositories.IUser;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Transactional
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private IUser repo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.mediasoft.services.demo.entities.User appUser = repo.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Login user invalid!"));
+        Set<GrantedAuthority> grantList = new HashSet<GrantedAuthority>();
+        for (Role role: appUser.getRoles()) {
+            GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(role.getDescription());
+            grantList.add(grantedAuthority);
+        }
+        UserDetails user = new User(username, appUser.getPassword(), grantList);
+        return user;
+    }
+}

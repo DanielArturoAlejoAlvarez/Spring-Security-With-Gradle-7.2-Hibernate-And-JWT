07dd2330dde5d947149a51c0ab065d892b8a8e20
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-12-30 20:03:36 -0500

    Creating WebSecurityConfig extends from WebSecurityConfigurerAdapter

---
+++ src/main/java/com/mediasoft/services/demo/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.mediasoft.services.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+}

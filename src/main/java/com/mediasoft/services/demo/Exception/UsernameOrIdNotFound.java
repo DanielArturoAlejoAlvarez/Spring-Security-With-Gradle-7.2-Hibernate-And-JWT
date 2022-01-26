7d3e6df187778207819ebaf0174082a502bd53f4
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2022-01-25 19:45:48 -0500

    Create UsernameOrIdNotFound method of Exception

--- src/main/java/com/mediasoft/services/demo/Exception/UsernameOrIdNotFound.java
+++ src/main/java/com/mediasoft/services/demo/Exception/UsernameOrIdNotFound.java
@@ -1,2 +1,13 @@
 package com.mediasoft.services.demo.Exception;
 
+public class UsernameOrIdNotFound extends Exception {
+    private static final long serialVersionUID = 1668398822129870029L;
+
+    public UsernameOrIdNotFound() {
+        super("Usuario o Id no encontrado");
+    }
+
+    public UsernameOrIdNotFound(String message) {
+        super(message);
+    }
+}
